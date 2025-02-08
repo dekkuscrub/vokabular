ef37f20b1575cebd2d321eebd4497630dce51d72
Björn Lundstedt <49694942+dekkuscrub@users.noreply.github.com>
2025-02-08 19:38:14 +0100

    Initialized Spring Boot project

---
+++ src/main/java/com/practice/vokabular/VokabularApplication.java
@@ -0,0 +1,13 @@
+package com.practice.vokabular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VokabularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VokabularApplication.class, args);
+	}
+
+}
